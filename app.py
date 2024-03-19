c82e3df59ef9f2fbfb76cc13687548fff058f423
Juan <juanpa.guayara@outlook.com>
2024-03-18 22:43:30 -0500

    Parcial funcionando faltan las pruebas unitarias

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-import boto3
-import requests
+import csv
 from datetime import datetime
+import io
+import boto3
+from bs4 import BeautifulSoup
+import requests
+
+s3 = boto3.client('s3')
+
 
 def lambda_handler(event, context):
     bucket_name = 'parcial1'
-    print("entro a la fincion")
+    print("entro a la función")
     for page_num in range(1, 6):
-        url = f'https://casas.mitula.com.co/searchRE/nivel1-Cundinamarca/nivel2-Bogot%C3%A1/orden-0/q-bogot%C3%A1/pag-{page_num}?req_sgmt=REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=='
+        url = f'https://casas.mitula.com.co/\
+                searchRE/nivel1-Cundinamarca/\
+                    nivel2-Bogot%C3%A1/orden-0/\
+                        q-bogot%C3%A1/pag-{page_num}?req_sgmt=\
+                            REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=='
         html_content = requests.get(url).content
 
         # Obtener la fecha actual
@@ -14,10 +24,73 @@ def lambda_handler(event, context):
 
         # Guardar el HTML en S3 con el formato deseado
         s3_key = f'casas/contenido-pag-{page_num}-{current_date}.html'
-        s3 = boto3.client('s3')
         s3.put_object(Bucket=bucket_name, Key=s3_key, Body=html_content)
 
     return {
         'statusCode': 200,
         'body': 'Páginas HTML descargadas y guardadas en S3.'
+    }
+
+
+def extraer(event, context):
+    # Extrae información relevante del evento
+    s3_event = event['Records'][0]['s3']
+    bucket_name = s3_event['bucket']['name']
+    object_key = s3_event['object']['key']
+
+    # Descarga el archivo HTML desde S3
+    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    html_content = response['Body'].read().decode('utf-8')
+
+    # Verificar que sea un archivo .html
+    if object_key.endswith('.html'):
+        # Extraer los datos con BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        propiedades = soup.find_all('div', class_='listing-card__title')
+        precios = soup.find_all('div', class_='price')
+        habitaciones = soup.find_all('span', {'data-test': 'bedrooms'})
+        metros_cuadrados = soup.find_all(
+            'div', class_='listing-card__property')
+
+        # Construye la ruta del archivo en el nuevo bucket
+        current_date = datetime.utcnow()
+        year, month, day = current_date.strftime(
+            '%Y'), current_date.strftime('%m'), current_date.strftime('%d')
+        new_bucket_name = 'bucket-finalnodamas'
+        new_folder_path = f'casas/year={year}/month={month}/day={day}/'
+        new_object_key = f'{new_folder_path}{year}-{month}-{day}.csv'
+
+        # Escribe los datos en un archivo CSV
+        csv_data = io.StringIO()
+        csv_writer = csv.writer(csv_data)
+        csv_writer.writerow(
+            ['Propiedad', 'Precio', 'Habitaciones', 'Metros_cuadrados'])
+
+        for propiedad, precio, habitacion, \
+            metro_cuadrado in zip(propiedades,
+                                  precios, habitaciones, metros_cuadrados):
+            propiedad = \
+                propiedad.text.\
+                strip().replace('\n', ' ') if propiedad else None
+            precio = precio.text.strip().replace('\n', ' ') if precio else None
+            habitacion = habitacion.text.strip().split()[0].replace(
+                '\n', ' ') if habitacion else None
+            metro_cuadrado = metro_cuadrado.find('span').text.strip().replace(
+                '\n', ' ') if metro_cuadrado else None
+
+            csv_writer.writerow(
+                [propiedad, precio, habitacion, metro_cuadrado])
+
+        # Guarda el archivo CSV en el nuevo bucket
+        s3.put_object(Bucket=new_bucket_name, Key=new_object_key,
+                      Body=csv_data.getvalue())
+
+        # Verificar si se necesita crear el directorio
+        if current_date.strftime('%Y-%m-%d') == f"{year}-{month}-{day}":
+            s3.put_object(Bucket=new_bucket_name, Key=new_folder_path, Body='')
+
+    return {
+        'statusCode': 200,
+        'body': 'Procesamiento exitoso'
     }
---
+++ test_app.py
@@ -0,0 +1,50 @@
+import pytest
+from datetime import datetime
+from moto import mock_s3
+from unittest.mock import MagicMock
+import requests
+import boto3
+
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    # Mockear la función requests.get
+    mocker.patch('requests.get')
+
+
+@pytest.fixture
+def mock_s3_client():
+    # Mockear el cliente de S3
+    with mock_s3():
+        yield
+
+
+def test_lambda_handler(mock_requests_get, mock_s3_client):
+
+    mocked_response = MagicMock()
+    mocked_response.content = b'<html><body>Mocked HTML</body></html>'
+    requests.get.return_value = mocked_response
+
+    # Llamar a la función lambda_handler
+    from app import lambda_handler  # Asegúrate de importar el módulo correcto
+    event = {}
+    context = {}
+    result = lambda_handler(event, context)
+
+    # Modifica la URL según sea necesario
+    requests.get.assert_called_with(
+        'https://casas.mitula.com.co/searchRE/nivel1-Cundinamarca/\
+            nivel2-Bogot%C3%A1/orden-0/q-bogot%C3%A1/\
+                pag-1?req_sgmt=REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw==')
+
+    # Verificar que los HTML se guardaron correctamente en S3
+    assert result['statusCode'] == 200
+    assert result['body'] == 'Páginas HTML descargadas y guardadas en S3.'
+
+    # Modificar según la estructura de tu bucket y la fecha actual
+    expected_s3_key = f'casas/contenido-pag-1-\
+        {datetime.now().strftime("%Y-%m-%d")}.html'
+    s3_client = boto3.client('s3')
+    response = s3_client.list_objects(Bucket='parcial1')
+    assert expected_s3_key in [obj['Key']
+                               for obj in response.get('Contents', [])]
